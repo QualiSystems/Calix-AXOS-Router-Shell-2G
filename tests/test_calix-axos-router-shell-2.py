af4e96ef732b9510855e4d60c1d227555b6b8754
Costya <costya.y@qualisystems.com>
2022-02-03 00:21:25 +0200

    initial commit

---
+++ tests/test_calix-axos-router-shell-2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for `CalixAxosRouterShell2Driver`
+"""
+
+import unittest
+
+from driver import CalixAxosRouterShell2Driver
+
+
+class TestCalixAxosRouterShell2Driver(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
