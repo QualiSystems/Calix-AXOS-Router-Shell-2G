8c3a640d2e361853a2833c07fc94592796d6b8e2
alexquali <alexey.b@quali.com>
2024-07-15 20:19:17 +0200

    CLI updated. Moved ot new structure

--- src/driver.py
+++ src/driver.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from cloudshell.cli.service.cli import CLI
+from cloudshell.cli.service.session_pool_manager import SessionPoolManager
 from cloudshell.shell.core.driver_context import (
     AutoLoadCommandContext,
     AutoLoadDetails,
@@ -9,6 +13,7 @@ from cloudshell.shell.core.orchestration_save_restore import OrchestrationSaveRe
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 from cloudshell.shell.core.session.logging_session import LoggingSessionContext
+from cloudshell.shell.flows.command.basic_flow import RunCommandFlow
 from cloudshell.shell.standards.networking.autoload_model import NetworkingResourceModel
 from cloudshell.shell.standards.networking.driver_interface import (
     NetworkingResourceDriverInterface,
@@ -16,23 +21,18 @@ from cloudshell.shell.standards.networking.driver_interface import (
 from cloudshell.shell.standards.networking.resource_config import (
     NetworkingResourceConfig,
 )
-
-from cloudshell.calix.cli.calix_cli_handler import CalixCli
-from cloudshell.calix.flows.calix_autoload_flow import (
-    CalixSnmpAutoloadFlow as AutoloadFlow,
+from cloudshell.snmp.snmp_configurator import EnableDisableSnmpConfigurator
+from cloudshell.calix.cli.calix_cli_configurator import CalixCliConfigurator
+from cloudshell.calix.flows.calix_autoload_flow import CalixSnmpAutoloadFlow
+from cloudshell.calix.flows.calix_configuration_flow import CalixConfigurationFlow
+from cloudshell.calix.flows.calix_enable_disable_snmp_flow import (
+    CalixEnableDisableSnmpFlow
 )
-from cloudshell.calix.flows.calix_configuration_flow import (
-    CalixConfigurationFlow as ConfigurationFlow,
-)
-from cloudshell.calix.flows.calix_run_command_flow import (
-    CalixCommandFlow as CommandFlow,
-)
-from cloudshell.calix.flows.calix_state_flow import CalixStateFlow as StateFlow
-from cloudshell.calix.snmp.calix_snmp_handler import CalixSnmpHandler as SNMPHandler
+from cloudshell.calix.flows.calix_state_flow import CalixStateFlow
 
 
 class CalixDriver(
-    ResourceDriverInterface, NetworkingResourceDriverInterface, GlobalLock
+    ResourceDriverInterface, NetworkingResourceDriverInterface
 ):
     SUPPORTED_OS = [r"Calix"]
     SHELL_NAME = "Calix AXOS Router 2G"
@@ -42,153 +42,103 @@ class CalixDriver(
         super(CalixDriver, self).__init__()
         self._cli = None
 
-    def initialize(self, context: InitCommandContext) -> str:
-        """Initialize method.
-
-        :param context: an object with all Resource Attributes inside
-        """
-        resource_config = NetworkingResourceConfig.from_context(
-            shell_name=self.SHELL_NAME, supported_os=self.SUPPORTED_OS, context=context
+    def initialize(self, context: InitCommandContext):
+        api = CloudShellSessionContext(context).get_api()
+        resource_config = NetworkingResourceConfig.from_context(context, api)
+        session_pool_size = int(resource_config.sessions_concurrency_limit)
+        self._cli = CLI(
+            SessionPoolManager(max_pool_size=session_pool_size, pool_timeout=100)
         )
-
-        self._cli = CalixCli(resource_config)
         return "Finished initializing"
 
-    def health_check(self, context: ResourceCommandContext):
-        """Performs device health check.
-
-        :param context: an object with all Resource Attributes inside
-        :return: Success or Error message
-        """
+    def health_check(self, context: ResourceCommandContext) -> bool:
+        """Performs device health check."""
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
 
-            state_operations = StateFlow(
-                logger=logger,
-                api=api,
-                resource_config=resource_config,
-                cli_configurator=cli_handler,
-            )
+            state_operations = CalixStateFlow(resource_config, cli_configurator, api)
             return state_operations.health_check()
 
     @GlobalLock.lock
     def get_inventory(self, context: AutoLoadCommandContext) -> AutoLoadDetails:
-        """Return device structure with all standard attributes.
-
-        :param context: an object with all Resource Attributes inside
-        :return: response
-        """
+        """Return device structure with all standard attributes."""
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
-            )
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            snmp_handler = SNMPHandler(resource_config, logger, cli_handler)
+            resource_config = NetworkingResourceConfig.from_context(context, api)
 
-            autoload_operations = AutoloadFlow(logger=logger, snmp_handler=snmp_handler)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
+            )
+            enable_disable_snmp_flow = CalixEnableDisableSnmpFlow(
+                cli_configurator,
+                resource_config.vrf_management_name
+            )
+            snmp_configurator = EnableDisableSnmpConfigurator.from_config(
+                enable_disable_snmp_flow, resource_config, logger
+            )
+
+            resource_model = NetworkingResourceModel.from_resource_config(
+                resource_config
+            )
+
+            autoload_operations = CalixSnmpAutoloadFlow(snmp_configurator)
             logger.info("Autoload started")
-            resource_model = NetworkingResourceModel(
-                resource_config.name,
-                resource_config.shell_name,
-                resource_config.family_name,
-            )
-
-            response = autoload_operations.discover(
-                resource_config.supported_os, resource_model
-            )
+            response = autoload_operations.discover(self.SUPPORTED_OS, resource_model)
             logger.info("Autoload completed")
             return response
 
     def ApplyConnectivityChanges(
-        self, context: ResourceCommandContext, request: str
+            self, context: ResourceCommandContext, request: str
     ) -> str:
-        """Create vlan and add or remove it to/from network interface.
-
-        :param context: an object with all Resource Attributes inside
-        :param str request: request json
-        :return:
-        """
+        """Create vlan and add or remove it to/from network interface."""
         pass
 
     def run_custom_command(
-        self, context: ResourceCommandContext, custom_command: str
+            self, context: ResourceCommandContext, custom_command: str
     ) -> str:
-        """Send custom command.
-
-        :param custom_command: Command user wants to send to the device.
-        :param context: an object with all Resource Attributes inside
-        :return: result
-        """
+        """Send custom command."""
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
 
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            send_command_operations = CommandFlow(
-                logger=logger, cli_configurator=cli_handler
-            )
-
-            response = send_command_operations.run_custom_command(
-                custom_command=custom_command
-            )
-
+            send_command_operations = RunCommandFlow(cli_configurator)
+            response = send_command_operations.run_custom_command(custom_command)
             return response
 
     def run_custom_config_command(
-        self, context: ResourceCommandContext, custom_command: str
+            self, context: ResourceCommandContext, custom_command: str
     ) -> str:
-        """Send custom command in configuration mode.
-
-        :param custom_command: Command user wants to send to the device
-        :param context: an object with all Resource Attributes inside
-        :return: result
-        """
+        """Send custom command in configuration mode."""
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
 
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            send_command_operations = CommandFlow(
-                logger=logger, cli_configurator=cli_handler
-            )
-
+            send_command_operations = RunCommandFlow(cli_configurator)
             result_str = send_command_operations.run_custom_config_command(
-                custom_command=custom_command
+                custom_command
             )
-
             return result_str
 
     def save(
-        self,
-        context: ResourceCommandContext,
-        folder_path: str,
-        configuration_type: str,
-        vrf_management_name: str,
+            self,
+            context: ResourceCommandContext,
+            folder_path: str,
+            configuration_type: str,
+            vrf_management_name: str,
     ) -> str:
         """Save selected file to the provided destination.
 
@@ -201,11 +151,9 @@ class CalixDriver(
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
 
             if not configuration_type:
@@ -214,12 +162,11 @@ class CalixDriver(
             if not vrf_management_name:
                 vrf_management_name = resource_config.vrf_management_name
 
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            configuration_flow = ConfigurationFlow(
-                cli_handler=cli_handler, logger=logger, resource_config=resource_config
+            configuration_operations = CalixConfigurationFlow(
+                resource_config, cli_configurator
             )
             logger.info("Save started")
-            response = configuration_flow.save(
+            response = configuration_operations.save(
                 folder_path=folder_path,
                 configuration_type=configuration_type,
                 vrf_management_name=vrf_management_name,
@@ -229,12 +176,12 @@ class CalixDriver(
 
     @GlobalLock.lock
     def restore(
-        self,
-        context: ResourceCommandContext,
-        path: str,
-        configuration_type: str,
-        restore_method: str,
-        vrf_management_name: str,
+            self,
+            context: ResourceCommandContext,
+            path: str,
+            configuration_type: str,
+            restore_method: str,
+            vrf_management_name: str,
     ):
         """Restore selected file to the provided destination.
 
@@ -247,11 +194,9 @@ class CalixDriver(
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
 
             if not configuration_type:
@@ -263,12 +208,11 @@ class CalixDriver(
             if not vrf_management_name:
                 vrf_management_name = resource_config.vrf_management_name
 
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            configuration_flow = ConfigurationFlow(
-                cli_handler=cli_handler, logger=logger, resource_config=resource_config
+            configuration_operations = CalixConfigurationFlow(
+                resource_config, cli_configurator
             )
             logger.info("Restore started")
-            configuration_flow.restore(
+            configuration_operations.restore(
                 path=path,
                 restore_method=restore_method,
                 configuration_type=configuration_type,
@@ -278,7 +222,7 @@ class CalixDriver(
 
     @GlobalLock.lock
     def load_firmware(
-        self, context: ResourceCommandContext, path: str, vrf_management_name: str
+            self, context: ResourceCommandContext, path: str, vrf_management_name: str
     ):
         """Upload and updates firmware on the resource.
 
@@ -289,7 +233,7 @@ class CalixDriver(
         pass
 
     def orchestration_save(
-        self, context: ResourceCommandContext, mode: str, custom_params: str
+            self, context: ResourceCommandContext, mode: str, custom_params: str
     ) -> str:
         """Save selected file to the provided destination.
 
@@ -298,39 +242,36 @@ class CalixDriver(
         :param custom_params: json with custom save parameters
         :return str response: response json
         """
-        with LoggingSessionContext(context) as logger:
-            if not mode:
-                mode = "shallow"
+        if not mode:
+            mode = "shallow"
 
+        with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
 
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            configuration_flow = ConfigurationFlow(
-                cli_handler=cli_handler, logger=logger, resource_config=resource_config
+            configuration_operations = CalixConfigurationFlow(
+                resource_config, cli_configurator
             )
 
             logger.info("Orchestration save started")
-            response = configuration_flow.orchestration_save(
+            response = configuration_operations.orchestration_save(
                 mode=mode, custom_params=custom_params
             )
             response_json = OrchestrationSaveRestore(
-                logger, resource_config.name
+                resource_config.name
             ).prepare_orchestration_save_result(response)
             logger.info("Orchestration save completed")
             return response_json
 
     def orchestration_restore(
-        self,
-        context: ResourceCommandContext,
-        saved_artifact_info: str,
-        custom_params: str,
+            self,
+            context: ResourceCommandContext,
+            saved_artifact_info: str,
+            custom_params: str,
     ):
         """Restore selected file to the provided destination.
 
@@ -341,23 +282,20 @@ class CalixDriver(
         with LoggingSessionContext(context) as logger:
             api = CloudShellSessionContext(context).get_api()
 
-            resource_config = NetworkingResourceConfig.from_context(
-                shell_name=self.SHELL_NAME,
-                supported_os=self.SUPPORTED_OS,
-                context=context,
-                api=api,
+            resource_config = NetworkingResourceConfig.from_context(context, api)
+            cli_configurator = CalixCliConfigurator.from_config(
+                resource_config, logger, self._cli
             )
 
-            cli_handler = self._cli.get_cli_handler(resource_config, logger)
-            configuration_flow = ConfigurationFlow(
-                cli_handler=cli_handler, logger=logger, resource_config=resource_config
+            configuration_operations = CalixConfigurationFlow(
+                resource_config, cli_configurator
             )
 
             logger.info("Orchestration restore started")
             restore_params = OrchestrationSaveRestore(
-                logger, resource_config.name
-            ).parse_orchestration_save_result(saved_artifact_info)
-            configuration_flow.restore(**restore_params)
+                resource_config.name
+            ).parse_orchestration_save_result(saved_artifact_info, custom_params)
+            configuration_operations.restore(**restore_params)
             logger.info("Orchestration restore completed")
 
     def shutdown(self, context):
